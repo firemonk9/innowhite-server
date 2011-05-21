230e17fe1248429673c4685317b8a3e4561224a8
firemonk <dhiraj.peechara@gmail.com>
2011-05-21 00:56:49 -0500

    change the default db server to dbserver and remapped in /etc/hosts

--- InnowhiteRed5/src/com/innowhite/red5/audio/FreeSwitchGateway.java
+++ InnowhiteRed5/src/com/innowhite/red5/audio/FreeSwitchGateway.java
@@ -35,283 +35,248 @@ import com.innowhite.red5.audio.events.ParticipantTalkingEvent;
  * @author leif
  */
 public class FreeSwitchGateway extends Observable implements IEslEventListener {
-	// private final Logger log = Red5LoggerFactory.getLogger(
-	// EslEventListener.class );
-	private static Logger log = Red5LoggerFactory.getLogger(
-			FreeSwitchGateway.class, "whiteboard");
+    // private final Logger log = Red5LoggerFactory.getLogger(
+    // EslEventListener.class );
+    private static Logger log = Red5LoggerFactory.getLogger(FreeSwitchGateway.class, "whiteboard");
 
-	private ManagerConnection managerConnection;
-	// private ConferenceEventListener conferenceEventListener;
-	private FreeswitchHeartbeatMonitor heartbeatMonitor;
-	static Map<String, Boolean> resultMap = new HashMap<String, Boolean>();
+    private ManagerConnection managerConnection;
+    // private ConferenceEventListener conferenceEventListener;
+    private FreeswitchHeartbeatMonitor heartbeatMonitor;
+    static Map<String, Boolean> resultMap = new HashMap<String, Boolean>();
 
-	static {
+    static {
 
-		resultMap.put("start-talking", true);
-		resultMap.put("stop-talking", false);
+	resultMap.put("start-talking", true);
+	resultMap.put("stop-talking", false);
 
+    }
+
+    public void startup() {
+
+	Client c = managerConnection.getESLClient();
+	c.addEventListener(this);
+	// c.cancelEventSubscriptions();
+	c.setEventSubscriptions("plain", "all");
+	c.addEventFilter("Event-Name", "heartbeat");
+	c.addEventFilter("Event-Name", "custom");
+	c.addEventFilter("Event-Name", "background_job");
+
+	try {
+	    Thread.sleep(5000);
+	} catch (InterruptedException ex) {
+	    java.util.logging.Logger.getLogger(FreeswitchHeartbeatMonitor.class.getName()).log(Level.SEVERE, null, ex);
 	}
 
-	public void startup() {
+	// Start Heartbeat and exception Event Observer Monitor
+	if (heartbeatMonitor == null) { // Only startup once. as startup will be
+					// called for reconnect.
+	    heartbeatMonitor = new FreeswitchHeartbeatMonitor(managerConnection, this);
+	    this.addObserver(heartbeatMonitor);
+	    heartbeatMonitor.start();
+	}
+    }
 
-		Client c = managerConnection.getESLClient();
-		c.addEventListener(this);
-		// c.cancelEventSubscriptions();
-		c.setEventSubscriptions("plain", "all");
-		c.addEventFilter("Event-Name", "heartbeat");
-		c.addEventFilter("Event-Name", "custom");
-		c.addEventFilter("Event-Name", "background_job");
+    public void setManagerConnection(ManagerConnection managerConnection) {
+	this.managerConnection = managerConnection;
+    }
 
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException ex) {
-			java.util.logging.Logger.getLogger(
-					FreeswitchHeartbeatMonitor.class.getName()).log(
-					Level.SEVERE, null, ex);
-		}
+    // public void eventReceived( EslEvent event )
+    // {
+    // log.info( "Event received [{}]", event );
+    // }
 
-		// Start Heartbeat and exception Event Observer Monitor
-		if (heartbeatMonitor == null) { // Only startup once. as startup will be
-										// called for reconnect.
-			heartbeatMonitor = new FreeswitchHeartbeatMonitor(
-					managerConnection, this);
-			this.addObserver(heartbeatMonitor);
-			heartbeatMonitor.start();
-		}
+    public void eventReceived(EslEvent event) {
+	if (event.getEventName().equals(FreeswitchHeartbeatMonitor.EVENT_HEARTBEAT)) {
+	    setChanged();
+	    notifyObservers(event);
+	    return; // No need to log.debug or process further the Observer will
+		    // act on this
+	}
+	// Ignored, Noop This is all the NON-Conference Events except Heartbeat
+	log.debug("eventReceived [{}]", event);
+
+    }
+
+    public void backgroundJobResultReceived(EslEvent event) {
+	log.info("Background job result received [{}]", event);
+    }
+
+    private AudioEventListener audioEventListener;
+
+    public AudioEventListener getAudioEventListener() {
+	return audioEventListener;
+    }
+
+    public void setAudioEventListener(AudioEventListener audioEventListener) {
+	this.audioEventListener = audioEventListener;
+    }
+
+    // @Override
+    public void mute(String confRoom, Integer participant, Boolean mute) {
+	MuteParticipantCommand mpc = new MuteParticipantCommand(confRoom, participant, mute, 0);
+	String jobId = managerConnection.getESLClient().sendAsyncApiCommand(mpc.getCommand(), mpc.getCommandArgs());
+	log.debug("mute called for room [{}] jobid [{}]", confRoom, jobId);
+    }
+
+    public void invalidConference(String confRoom, Integer participant, Boolean mute) {
+	MuteParticipantCommand mpc = new MuteParticipantCommand(confRoom, participant, mute, 0);
+	String jobId = managerConnection.getESLClient().sendAsyncApiCommand(mpc.getCommand(), mpc.getCommandArgs());
+	log.debug("mute called for room [{}] jobid [{}]", confRoom, jobId);
+    }
+
+    // @Override
+    public void eject(String confRoom, Integer participant) {
+	EjectParticipantCommand mpc = new EjectParticipantCommand(confRoom, participant, 0);
+	String jobId = managerConnection.getESLClient().sendAsyncApiCommand(mpc.getCommand(), mpc.getCommandArgs());
+	log.debug("eject/kick called for room [{}]  participant [{}]", confRoom, participant);
+
+    }
+
+    // added innoUserUnique parameter
+    public void conferenceEventJoin(String uniqueId, String confName, int confSize, EslEvent event) {
+
+	Integer memberId = this.getMemeberIdFromEvent(event);
+	StringBuilder sb = new StringBuilder("");
+
+	String innoUserUnique = getInnoUniqueIdFromEvent(event);
+	String callerUsername = getInnoUniqueIdFromEvent(event);
+
+	sb.append(uniqueId);
+	Object[] args = new Object[5];
+	args[0] = confName;
+	args[1] = confSize;
+	args[2] = sb.toString();
+	args[3] = innoUserUnique;
+	args[4] = callerUsername;
+
+	// IConnection con = Red5.getConnectionLocal();
+
+	// Red5.getConnectionLocal().get
+	ParticipantJoinedEvent pj = new ParticipantJoinedEvent("" + memberId, confName, callerUsername, innoUserUnique, true, true);
+	audioEventListener.handleConferenceEvent(pj);
+
+	log.info("Conference join: uniqueId " + uniqueId + "  confName " + confName + " confSize " + confSize + "  innoUserUnique " + innoUserUnique + "  callerUsername  " + callerUsername
+		+ "   event  " + event);
+
+	// log.info ("Conference [{}]({}) JOIN [{}] InnoUser [{}] source [{}]",
+	// args);
+    }
+
+    public void conferenceEventLeave(String uniqueId, String confName, int confSize, EslEvent event) {
+
+	Integer memberId = this.getMemeberIdFromEvent(event);
+	StringBuilder sb = new StringBuilder("");
+	sb.append(uniqueId);
+	Object[] args = new Object[3];
+	args[0] = confName;
+	args[1] = confSize;
+	args[2] = sb.toString();
+
+	ParticipantLeftEvent pj = new ParticipantLeftEvent("" + memberId, confName);
+	audioEventListener.handleConferenceEvent(pj);
+
+	log.info("Conference Leave: uniqueId " + uniqueId + "  confName " + confName + " confSize " + confSize + " event " + event);
+	// log.info ("Conference [{}]({}) LEAVE [{}]", args);
+    }
+
+    public void conferenceEventMute(String uniqueId, String confName, int confSize, EslEvent event) {
+
+	Integer memberId = this.getMemeberIdFromEvent(event);
+	StringBuilder sb = new StringBuilder("");
+	sb.append(uniqueId);
+
+	ParticipantMutedEvent pj = new ParticipantMutedEvent("" + memberId, confName, true);
+	audioEventListener.handleConferenceEvent(pj);
+
+	log.debug("Conference [{}] MUTE [{}]", confName, sb.toString());
+    }
+
+    public void conferenceEventUnMute(String uniqueId, String confName, int confSize, EslEvent event) {
+
+	Integer memberId = this.getMemeberIdFromEvent(event);
+	StringBuilder sb = new StringBuilder("");
+	sb.append(uniqueId);
+
+	ParticipantMutedEvent pj = new ParticipantMutedEvent("" + memberId, confName, true);
+	audioEventListener.handleConferenceEvent(pj);
+
+	log.debug("Conference [{}] UNMUTE [{}]", confName, sb.toString());
+    }
+
+    public void conferenceEventAction(String uniqueId, String confName, int confSize, String action, EslEvent event) {
+
+	Integer memberId = this.getMemeberIdFromEvent(event);
+	StringBuilder sb = new StringBuilder("");
+	sb.append("" + memberId);
+	sb.append(" action=[");
+	sb.append(action);
+	sb.append("]");
+
+	// log.debug("Conference leave: "+sb);
+	log.debug("Conference Action talk: memberId " + memberId + "  confName " + confName + " confSize " + confSize + " event " + event + "  action " + action);
+
+	if (resultMap.get(action) != null) {
+	    ParticipantTalkingEvent pj = new ParticipantTalkingEvent("" + memberId, confName, resultMap.get(action));
+	    audioEventListener.handleConferenceEvent(pj);
+
+	    log.debug("Conference Action talk: memberId " + memberId + "  confName " + confName + " confSize " + confSize + " event " + event + "  action " + action);
+	}
+	// log.info ("Conference [{}] Action [{}]", confName, sb.toString());
+    }
+
+    public void conferenceEventTransfer(String uniqueId, String confName, int confSize, EslEvent event) {
+	// Noop
+    }
+
+    public void conferenceEventThreadRun(String uniqueId, String confName, int confSize, EslEvent event) {
+	// --Conference-Recorded-File-End
+	// --Conference-Recorded-File-Begin
+
+	log.debug("Entered conferenceEventThreadRun uniqueId::" + uniqueId + " confName::" + confName + " memberID " + getMemeberIdFromEvent(event));
+	Integer memberId = this.getMemeberIdFromEvent(event);
+	String startFile = getStartFileFromEvent(event);
+	String endFile = getEndFileFromEvent(event);
+	if (startFile != null || endFile != null) {
+	    AudioFileStartStopEvent ae = new AudioFileStartStopEvent("" + memberId, confName, startFile, endFile);
+	    audioEventListener.handleConferenceEvent(ae);
 	}
 
-	public void setManagerConnection(ManagerConnection managerConnection) {
-		this.managerConnection = managerConnection;
+    }
+
+    public void conferenceEventPlayFile(String uniqueId, String confName, int confSize, EslEvent event) {
+	// Noop
+    }
+
+    public void exceptionCaught(ExceptionEvent e) {
+	// throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private String getStartFileFromEvent(EslEvent e) {
+	return (e.getEventHeaders().get("Conference-Recorded-File-Begin"));
+    }
+
+    private String getEndFileFromEvent(EslEvent e) {
+	return (e.getEventHeaders().get("Conference-Recorded-File-End"));
+    }
+
+    private String getInnoUniqueIdFromEvent(EslEvent e) {
+	return (e.getEventHeaders().get("Inno-Unique-ID"));
+    }
+
+    private Integer getMemeberIdFromEvent(EslEvent e) {
+	try {
+	    if (e != null)
+		return new Integer(e.getEventHeaders().get("Member-ID"));
+
+	} catch (Exception ex) {
+	    log.warn("member id is null..");
 	}
+	return 0;
+    }
 
-	// public void eventReceived( EslEvent event )
-	// {
-	// log.info( "Event received [{}]", event );
-	// }
+    public void shutdown() {
+	// TODO Auto-generated method stub
 
-	public void eventReceived(EslEvent event) {
-		if (event.getEventName().equals(
-				FreeswitchHeartbeatMonitor.EVENT_HEARTBEAT)) {
-			setChanged();
-			notifyObservers(event);
-			return; // No need to log.debug or process further the Observer will
-					// act on this
-		}
-		// Ignored, Noop This is all the NON-Conference Events except Heartbeat
-		log.debug("eventReceived [{}]", event);
-
-	}
-
-	public void backgroundJobResultReceived(EslEvent event) {
-		log.info("Background job result received [{}]", event);
-	}
-
-	private AudioEventListener audioEventListener;
-
-	public AudioEventListener getAudioEventListener() {
-		return audioEventListener;
-	}
-
-	public void setAudioEventListener(AudioEventListener audioEventListener) {
-		this.audioEventListener = audioEventListener;
-	}
-
-	// @Override
-	public void mute(String confRoom, Integer participant, Boolean mute) {
-		MuteParticipantCommand mpc = new MuteParticipantCommand(confRoom,
-				participant, mute, 0);
-		String jobId = managerConnection.getESLClient().sendAsyncApiCommand(
-				mpc.getCommand(), mpc.getCommandArgs());
-		log.debug("mute called for room [{}] jobid [{}]", confRoom, jobId);
-	}
-
-	
-
-	public void invalidConference(String confRoom, Integer participant, Boolean mute) {
-		MuteParticipantCommand mpc = new MuteParticipantCommand(confRoom,
-				participant, mute, 0);
-		String jobId = managerConnection.getESLClient().sendAsyncApiCommand(
-				mpc.getCommand(), mpc.getCommandArgs());
-		log.debug("mute called for room [{}] jobid [{}]", confRoom, jobId);
-	}
-	
-	
-	// @Override
-	public void eject(String confRoom, Integer participant) {
-		EjectParticipantCommand mpc = new EjectParticipantCommand(confRoom,
-				participant, 0);
-		String jobId = managerConnection.getESLClient().sendAsyncApiCommand(
-				mpc.getCommand(), mpc.getCommandArgs());
-		log.debug("eject/kick called for room [{}]  participant [{}]",
-				confRoom, participant);
-
-	}
-
-	// added innoUserUnique parameter
-	public void conferenceEventJoin(String uniqueId, String confName,
-			int confSize, EslEvent event) {
-
-		Integer memberId = this.getMemeberIdFromEvent(event);
-		StringBuilder sb = new StringBuilder("");
-
-		String innoUserUnique = getInnoUniqueIdFromEvent(event);
-		String callerUsername = getInnoUniqueIdFromEvent(event);
-
-		sb.append(uniqueId);
-		Object[] args = new Object[5];
-		args[0] = confName;
-		args[1] = confSize;
-		args[2] = sb.toString();
-		args[3] = innoUserUnique;
-		args[4] = callerUsername;
-
-		// IConnection con = Red5.getConnectionLocal();
-
-		// Red5.getConnectionLocal().get
-		ParticipantJoinedEvent pj = new ParticipantJoinedEvent("" + memberId,
-				confName, callerUsername, innoUserUnique, true, true);
-		audioEventListener.handleConferenceEvent(pj);
-
-		log.info("Conference join: uniqueId " + uniqueId + "  confName "
-				+ confName + " confSize " + confSize + "  innoUserUnique "
-				+ innoUserUnique + "  callerUsername  " + callerUsername
-				+ "   event  " + event);
-
-		// log.info ("Conference [{}]({}) JOIN [{}] InnoUser [{}] source [{}]",
-		// args);
-	}
-
-	public void conferenceEventLeave(String uniqueId, String confName,
-			int confSize, EslEvent event) {
-
-		Integer memberId = this.getMemeberIdFromEvent(event);
-		StringBuilder sb = new StringBuilder("");
-		sb.append(uniqueId);
-		Object[] args = new Object[3];
-		args[0] = confName;
-		args[1] = confSize;
-		args[2] = sb.toString();
-
-		ParticipantLeftEvent pj = new ParticipantLeftEvent("" + memberId,
-				confName);
-		audioEventListener.handleConferenceEvent(pj);
-
-		log.info("Conference Leave: uniqueId " + uniqueId + "  confName "
-				+ confName + " confSize " + confSize + " event " + event);
-		// log.info ("Conference [{}]({}) LEAVE [{}]", args);
-	}
-
-	public void conferenceEventMute(String uniqueId, String confName,
-			int confSize, EslEvent event) {
-
-		Integer memberId = this.getMemeberIdFromEvent(event);
-		StringBuilder sb = new StringBuilder("");
-		sb.append(uniqueId);
-
-		ParticipantMutedEvent pj = new ParticipantMutedEvent("" + memberId,
-				confName, true);
-		audioEventListener.handleConferenceEvent(pj);
-
-		log.debug("Conference [{}] MUTE [{}]", confName, sb.toString());
-	}
-
-	public void conferenceEventUnMute(String uniqueId, String confName,
-			int confSize, EslEvent event) {
-
-		Integer memberId = this.getMemeberIdFromEvent(event);
-		StringBuilder sb = new StringBuilder("");
-		sb.append(uniqueId);
-
-		ParticipantMutedEvent pj = new ParticipantMutedEvent("" + memberId,
-				confName, true);
-		audioEventListener.handleConferenceEvent(pj);
-
-		log.debug("Conference [{}] UNMUTE [{}]", confName, sb.toString());
-	}
-
-	public void conferenceEventAction(String uniqueId, String confName,
-			int confSize, String action, EslEvent event) {
-
-		Integer memberId = this.getMemeberIdFromEvent(event);
-		StringBuilder sb = new StringBuilder("");
-		sb.append("" + memberId);
-		sb.append(" action=[");
-		sb.append(action);
-		sb.append("]");
-
-		// log.debug("Conference leave: "+sb);
-		log.debug("Conference Action talk: memberId " + memberId
-				+ "  confName " + confName + " confSize " + confSize
-				+ " event " + event + "  action " + action);
-
-		if (resultMap.get(action) != null) {
-			ParticipantTalkingEvent pj = new ParticipantTalkingEvent(""
-					+ memberId, confName, resultMap.get(action));
-			audioEventListener.handleConferenceEvent(pj);
-
-			log.debug("Conference Action talk: memberId " + memberId
-					+ "  confName " + confName + " confSize " + confSize
-					+ " event " + event + "  action " + action);
-		}
-		// log.info ("Conference [{}] Action [{}]", confName, sb.toString());
-	}
-
-	public void conferenceEventTransfer(String uniqueId, String confName,
-			int confSize, EslEvent event) {
-		// Noop
-	}
-
-	public void conferenceEventThreadRun(String uniqueId, String confName,
-			int confSize, EslEvent event) {
-		// --Conference-Recorded-File-End
-		// --Conference-Recorded-File-Begin
-		
-		log.debug("Entered conferenceEventThreadRun uniqueId::"+uniqueId+" confName::"+confName+" memberID "+getMemeberIdFromEvent(event));
-		Integer memberId = this.getMemeberIdFromEvent(event);
-		String startFile = getStartFileFromEvent(event);
-		String endFile = getEndFileFromEvent(event);
-		if (startFile != null || endFile != null) {
-			AudioFileStartStopEvent ae = new AudioFileStartStopEvent(""
-					+ memberId, confName, startFile, endFile);
-			audioEventListener.handleConferenceEvent(ae);
-		}
-
-	}
-
-	public void conferenceEventPlayFile(String uniqueId, String confName,
-			int confSize, EslEvent event) {
-		// Noop
-	}
-
-	public void exceptionCaught(ExceptionEvent e) {
-		// throw new UnsupportedOperationException("Not supported yet.");
-	}
-
-	private String getStartFileFromEvent(EslEvent e) {
-		return (e.getEventHeaders().get("Conference-Recorded-File-Begin"));
-	}
-
-	private String getEndFileFromEvent(EslEvent e) {
-		return (e.getEventHeaders().get("Conference-Recorded-File-End"));
-	}
-
-	private String getInnoUniqueIdFromEvent(EslEvent e) {
-		return (e.getEventHeaders().get("Inno-Unique-ID"));
-	}
-
-	private Integer getMemeberIdFromEvent(EslEvent e) {
-		try {
-			return new Integer(e.getEventHeaders().get("Member-ID"));
-
-		} catch (Exception ex) {
-			log.warn("member id is null..");
-		}
-		return 0;
-	}
-
-	public void shutdown() {
-		// TODO Auto-generated method stub
-
-	}
+    }
 
 }
