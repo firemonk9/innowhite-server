6792f89d4f5fee8a203fd1a35915344502722646
Dhiraj Peechara <Dhiraj.Peechara@gmail.com>
2011-11-03 22:09:03 -0400

    moved image magic processing code to new class.

--- BatchWebProject/src/main/java/com/innowhite/PlaybackApp/service/PlaybackDataService.java
+++ BatchWebProject/src/main/java/com/innowhite/PlaybackApp/service/PlaybackDataService.java
@@ -120,6 +120,14 @@ public class PlaybackDataService {
 	    // TODO remove videos with (<=)3seconds duration
 	    List<VideoData> videoDataList = videoDataDao.getVideoDataList(roomId);
 	    for (int i = 0; i < videoDataList.size(); i++) {
+		
+		if(videoDataList.get(i) == null || videoDataList.get(i).getStartTime() == null || videoDataList.get(i).getEndTime() == null)
+		{
+		    log.warn(" video cannot have bacis information null so exiting the process.  ");
+		    log.warn(" Getting invalid information for video obj "+videoDataList.get(i));
+		    
+		    return;
+		}
 		long vS = videoDataList.get(i).getStartTime().getTime();
 		long vE = videoDataList.get(i).getEndTime().getTime();
 		if ((vE - vS) < 3) {
@@ -315,13 +323,13 @@ public class PlaybackDataService {
 			    // uniformSessionVideoDataList =
 			    // setVideoFormatResolution(paddedSessionVideoPlaylist,
 			    // videoDimensions);
-			    uniformSessionVideoDataList = formatSessionVideoPlaylist(paddedSessionVideoDatalist, maxVideoDimensions);
+			    uniformSessionVideoDataList = VideoImageMagick.formatSessionVideoPlaylist(paddedSessionVideoDatalist, maxVideoDimensions,playbackVO);
 			} else {
 			    // Set resolution of all Session Bucket Videos
 			    // uniformSessionVideoDataList =
 			    // setVideoFormatResolution(sessionVideoDataList,
 			    // videoDimensions);
-			    uniformSessionVideoDataList = formatSessionVideoPlaylist(sessionVideoDataList, maxVideoDimensions);
+			    uniformSessionVideoDataList = VideoImageMagick.formatSessionVideoPlaylist(sessionVideoDataList, maxVideoDimensions,playbackVO);
 			}
 			log.debug("_______________________________________________________________");
 			log.debug("Number of videos after setting resolution :: " + uniformSessionVideoDataList.size());
@@ -401,80 +409,8 @@ public class PlaybackDataService {
 	}
     }
 
-    private List<VideoData> formatSessionVideoPlaylist(List<VideoData> paddedSessionVideoDatalist, String maxVideoDimensions) {
-	log.debug("Inside formatSessionVideoPlaylist...");
-	// TODO convert videos to images
-	List<VideoData> tempVideoDataList = paddedSessionVideoDatalist;
-	String cmd = null;
-	int duration;
-	String uniquePath = PlaybackUtil.getUnique();
 
-	log.debug("creating black background image..");
-	cmd = " -size " + maxVideoDimensions + " xc:black " + playbackVO.getTempLocation() + "/backgroundImage" + uniquePath + ".jpg";
-	PlaybackUtil.invokeImageMagickProcess(cmd);
-	for (int i = 0; i < tempVideoDataList.size(); i++) {
-	    log.debug("convert video to images..");
-
-	    String strDirectoy = playbackVO.getTempLocation() + "/sessionVideo" + uniquePath;
-	    // TODO Create random sessionVideo directory
-	    boolean success = createDir(strDirectoy);
-	    if (success == false) {
-		log.warn(" Could not create the directory.... returnign  ");
-		return null;
-
-	    }
-
-	    cmd = " -i " + tempVideoDataList.get(i).getFilePath() + " -r 2 -f image2 " + strDirectoy + "/%05d.jpg";
-	    PlaybackUtil.invokeFfmpegProcess(cmd);
-
-	    HashMap<String, String> videohm1 = new HashMap<String, String>();
-	    cmd = " -i " + tempVideoDataList.get(i).getFilePath();
-	    PlaybackUtil.invokeVideoAttribProcess(cmd, videohm1);
-	    duration = PlaybackUtil.getNum(videohm1.get("duration"));
-	    log.debug("duration" + duration);
-
-	    // TODO compose all images to a max width:height black background
-	    // image
-	    log.debug("compose all images to a max width:height black background image");
-	    for (int j = 1; j <= duration * 2; j++) {
-		// TODO check if file exists
-		File f = new File(strDirectoy + "/" + String.format("%05d", j) + ".jpg");
-		if (f.exists()) {
-		    // log.debug("converting ");
-			String biPath = playbackVO.getTempLocation() + "/backgroundImage" + uniquePath + ".jpg";
-			String imPath = strDirectoy + "/" + String.format("%05d", j) + ".jpg";
-			//convert composite.jpg -gravity Center -draw "image Over 0,0 0,0 '00019.jpg'" zzzzzz.jpg
-		    cmd = " "+biPath+" -gravity Center -draw \"image Over 0,0 0,0 '"+imPath+"'\" "+imPath;
-		    log.debug("Composing "+imPath);
-		    PlaybackUtil.invokeImageMagickProcess(cmd);
-		} else {
-		    log.warn("file/image does not exist.. exiting.." + f.getAbsolutePath());
-		}
-	    }
-
-	    // TODO convert images to videos
-	    log.debug("convert images to videos");
-	    cmd = " -y -r 2 -i " + strDirectoy + "/%05d.jpg -an " + paddedSessionVideoDatalist.get(i).getFilePath();
-	    PlaybackUtil.invokeFfmpegProcess(cmd);
-	    // tempVideoDataList.remove(i);
-	    // vd.setFilePath(tempVideoDataList.get(i).get);
-	    // tempVideoDataList.add(i,
-	    // paddedSessionVideoDatalist.get(i).getFilePath());
-	}
-	return tempVideoDataList;
-    }
-
-    private boolean createDir(String strDirectoy) {
-
-	boolean success = (new File(strDirectoy)).mkdir();
-	if (success) {
-	    log.debug("Random sessionVideo Directory: " + strDirectoy + " created!");
-	} else {
-	    log.warn("directory not created. may already exist" + strDirectoy);
-	}
-	return success;
-
-    }
+   
 
     private PlayBackPlayList setPlayBackPlayList(String videoPath, String roomId) {
 	HashMap<String, String> videohm1 = new HashMap<String, String>();
