1943028a410df92f7074ff3c43fefcb00d95ce40
Dhiraj Peechara <Dhiraj.Peechara@gmail.com>
2011-12-11 00:06:49 -0500

    Getting the room Name and description from the web Page.

---
+++ BatchWebProject/src/main/java/com/innowhite/PlaybackApp/dao/RoomDao.java
@@ -0,0 +1,53 @@
+package com.innowhite.PlaybackApp.dao;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.innowhite.PlaybackApp.model.RoomData;
+
+public class RoomDao {
+    SessionFactory sessionFactory;
+
+    private static final Logger log = LoggerFactory.getLogger(RoomDao.class);
+
+
+    public static void main(String args[]){
+	
+	
+    }
+    
+    public void setSessionFactory(SessionFactory sessionFactory) {
+	this.sessionFactory = sessionFactory;
+    }
+    
+    @Transactional
+    public String getSessionDetailXML(String roomId) {
+
+	log.debug("Entered getSessionDetailXML with roomId :"+roomId);
+	Session session = sessionFactory.getCurrentSession();
+
+	Criteria crit = session.createCriteria(RoomData.class);
+	@SuppressWarnings("unchecked")
+	List<RoomData> list2 = crit.add(Restrictions.eq("roomName", roomId)).list();
+
+	session.clear();
+	session.flush();
+	
+	if(list2 != null && list2.size() ==1 ){
+	    RoomData data = list2.get(0);
+	    if(data != null)
+		return data.getRoomDetailXml();
+	}
+	log.warn("The xml is null so setting default name for the uploaded video.");
+	return null;
+
+    }
+    
+}
---
+++ BatchWebProject/src/main/java/com/innowhite/PlaybackApp/model/RoomData.java
@@ -0,0 +1,75 @@
+package com.innowhite.PlaybackApp.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "room")
+public class RoomData implements Serializable {
+
+    /**
+	 * 
+	 */
+   // private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "room_id")
+    String roomName;
+   
+
+    @Column(name = "room_detail_xml")
+    String roomDetailXml;
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    
+    public String getRoomDetailXml() {
+        return roomDetailXml;
+    }
+
+    public void setRoomDetailXml(String roomDetailXml) {
+        this.roomDetailXml = roomDetailXml;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public Long getId() {
+	return id;
+    }
+
+    public static void main(String[] args) {
+	//whiteboard
+    }
+
+    
+ public String toString(){
+	
+	StringBuffer sb = new StringBuffer();
+	sb.append(" id: "+id);
+	
+	sb.append(" roomName: "+roomName);
+	
+	
+	
+	return sb.toString();
+    }
+    
+}
