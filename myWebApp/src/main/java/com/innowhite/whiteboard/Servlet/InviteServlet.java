7c7cf456691a2536fde6ad52283aa80a0a5688f6
Dhiraj Peechara <Dhiraj.Peechara@gmail.com>
2012-02-17 21:35:50 -0500

    changed the xml for invite participants

--- myWebApp/src/main/java/com/innowhite/whiteboard/Servlet/InviteServlet.java
+++ myWebApp/src/main/java/com/innowhite/whiteboard/Servlet/InviteServlet.java
@@ -15,38 +15,47 @@ import com.innowhite.whiteboard.service.SendInviteService;
  * Servlet implementation class InviteServlet
  */
 public class InviteServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
+	private static final long serialVersionUID = 1L;
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public InviteServlet() {
-	super();
-	// TODO Auto-generated constructor stub
-    }
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	// TODO Auto-generated method stub
-    }
-
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	String xml = request.getParameter("xml");
-	//String roomId = request.getParameter("roomId");
-	log.debug(" The xml from flex client is :: " + xml);
-	if(xml != null){
-	    
-	    SendInviteService.sendInviteService(xml);
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public InviteServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
-    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String roomId = request.getParameter("roomId");
+		String list = request.getParameter("list");
+		
+		log.debug("Entered Post Method list:"+list+" roomId : "+roomId);
+		// String roomId = request.getParameter("roomId");
+		// log.debug(" The xml from flex client is :: " + xml);
+		if (roomId != null && list != null) {
+			StringBuffer sb = new StringBuffer();
+			sb.append("<?xml version='1.0' encoding='UTF-8'?>");
+			sb.append("<invitees>");
+			sb.append("<roomId>"+roomId+"</roomId>");
+			sb.append("<list>" + list + "</list>");
+			sb.append("</invitees>");
+			SendInviteService.sendInviteService(sb.toString());
+
+		}
+
+	}
 
 }
