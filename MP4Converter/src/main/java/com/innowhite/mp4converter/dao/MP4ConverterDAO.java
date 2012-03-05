766b2418e255e4a7e8145134dfb3e99eb84b85bd
altanuja <altanuja@gmail.com>
2012-03-05 14:57:23 -0500

    MP4 file path updated in DB playback_playlist table

--- MP4Converter/src/main/java/com/innowhite/mp4converter/dao/MP4ConverterDAO.java
+++ MP4Converter/src/main/java/com/innowhite/mp4converter/dao/MP4ConverterDAO.java
@@ -1,5 +1,39 @@
 package com.innowhite.mp4converter.dao;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.classic.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.innowhite.mp4converter.model.PlayBackPlayList;
+
 public class MP4ConverterDAO {
 
+	private static final Logger log = LoggerFactory.getLogger(MP4ConverterDAO.class);
+	SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
+	@Transactional
+	public void updateMp4FilePath(PlayBackPlayList playBackObj){
+		log.debug("entered updateMp4FilePath");
+		int returnVal=0;
+		try {
+			Session session = sessionFactory.getCurrentSession();
+
+			String query = "update PlayBackPlayList pp set pp.downloadUrl=:mp4FilePath  where pp.id=:fileId";
+			returnVal = session.createQuery(query).setString("mp4FilePath", playBackObj.getDownloadUrl())
+					.setString("fileId", playBackObj.getId()).executeUpdate();
+			session.clear();
+			session.flush();
+			log.debug("retuned updateMp4FilePath val " + returnVal);
+		} catch (Exception e) {
+		    e.printStackTrace();
+		}
+	}
+	
+	
 }
--- MP4Converter/src/main/java/com/innowhite/mp4converter/service/MP4ConverterService.java
+++ MP4Converter/src/main/java/com/innowhite/mp4converter/service/MP4ConverterService.java
@@ -3,8 +3,12 @@ package com.innowhite.mp4converter.service;
 
 import java.io.File;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.innowhite.mp4converter.dao.MP4ConverterDAO;
 import com.innowhite.mp4converter.model.FFMPEGInfoVO;
+import com.innowhite.mp4converter.model.PlayBackPlayList;
 import com.innowhite.mp4converter.util.ConverterUtil;
 
 /**
@@ -13,54 +17,49 @@ import com.innowhite.mp4converter.util.ConverterUtil;
  */
 
 public class MP4ConverterService {
-
-	private MP4ConverterDAO mp4COnverterDAO = null;	
+	
+	private static final Logger log = LoggerFactory.getLogger(MP4ConverterService.class);
+	private MP4ConverterDAO mp4ConverterDAO = null;	
 	private FFMPEGInfoVO ffmpegInfoVO = null;
 	
 	public MP4ConverterDAO getmp4ConverterDAO() {
-		return mp4COnverterDAO;
+		return mp4ConverterDAO;
 	}
 
-	public void setmp4ConverterDAO(MP4ConverterDAO mp4COnverterDAO) {
-		this.mp4COnverterDAO = mp4COnverterDAO;
+	public void setmp4ConverterDAO(MP4ConverterDAO mp4ConverterDAO) {
+		this.mp4ConverterDAO = mp4ConverterDAO;
 	}
 
 	public FFMPEGInfoVO getffmpegInfoVO() {
 		return ffmpegInfoVO;
 	}
-
 	public void setffmpegInfoVO(FFMPEGInfoVO playbackVO) {
 		this.ffmpegInfoVO = ffmpegInfoVO;
 	}	
 	
-	/*public static void main(String args[]){
-			
-			//convertToMp4();
-	}*/
 		
 	public void processFLVFile(String inputFilePath, String fileId, String winPath){
-		
+		log.debug("entered processFLVFile");
 		if(ConverterUtil.isWindows()==true){
-			convertToMp4(inputFilePath);
+			convertToMp4(inputFilePath, fileId);
 		}
-			
 	}
 		
-	public void convertToMp4(String inputFLVPath){
+	public void convertToMp4(String inputFLVPath, String fileId){
+		log.debug("entered convertToMp4");
 			
 		try{
-			
 			//inputFLVPath="C:/FLVVideoFiles/test.flv";
 			//String ffmpegPath = "C:\\Program Files\\ffmpeg-git-b6ff81d-win32-static\\bin\\ffmpeg.exe";
+			String outMp4FilePath = inputFLVPath.replace(".flv", ".mp4");
 				
 			String ffmpegPath=ffmpegInfoVO.getWinFFmpegPath();
-			String tempFilePath = "C:/FLVVideoFiles/test.mp4";
 										
 			File myFile = new File(inputFLVPath);
 				
 			if (myFile.exists()) {
 					
-				String command = ffmpegPath+" -i "+inputFLVPath+" C:/FLVVideoFiles/test.mp4";
+				String command = ffmpegPath+" -i "+inputFLVPath+" "+outMp4FilePath;
 				
 				System.out.println("Before dispatching command...");
 						
@@ -70,10 +69,26 @@ public class MP4ConverterService {
 			}else{
 				System.out.println("There is no file to convert");
 			}
+			
+			updateFinalVideoTable(fileId, outMp4FilePath);
 				
 		}catch(Exception e){
 			e.printStackTrace();
 		}
+		log.debug("leaving convertToMp4  ");
+		
+	}
+	
+	
+	public void updateFinalVideoTable(String strFileId, String mp4FilePath){
+		log.debug("entered updateFinalVideoTable");
+			
+		PlayBackPlayList playBackObj = new PlayBackPlayList();
+		playBackObj.setDownloadUrl(mp4FilePath);
+		playBackObj.setId(strFileId);
+		
+		mp4ConverterDAO.updateMp4FilePath(playBackObj);
+		log.debug("leaving updateFinalVideoTable  ");
 	}
 
 }
